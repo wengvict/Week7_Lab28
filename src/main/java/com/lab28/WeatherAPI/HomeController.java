3b580a56ef5e3aa946cc02fabeeeedd86e0148cb
Victoria Weng <wengvict@gmail.com>
2019-06-06 09:22:51 -0400

    Add form to search by lat/long

--- src/main/java/com/lab28/WeatherAPI/HomeController.java
+++ src/main/java/com/lab28/WeatherAPI/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -36,6 +37,17 @@ public class HomeController {
 		System.out.println(response.getBody().getData().getWeather());
 				
 		return new ModelAndView("index", "forecast", response.getBody().getData().getText());
+	}
+	
+	@RequestMapping("/location-form")
+	public ModelAndView findLocation(@RequestParam("lat") Double lat, @RequestParam("lon") Double lon) {
+		
+		RestTemplate rt = new RestTemplate();
+
+		HttpEntity<Weather> entity = null;
+		String url = "https://forecast.weather.gov/MapClick.php?lat=" + lat + "&lon=" + lon + "&FcstType=json";
+		ResponseEntity<Weather> response = rt.exchange(url, HttpMethod.GET, entity, Weather.class);
+		return new ModelAndView("redirect:/", "forecastLocation", response.getBody().getData().getText());
 	}
 
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -8,9 +8,18 @@
 <title>Insert title here</title>
 </head>
 <body>
+	
+	<h1>Find the Weather for Your City</h1>
 
- 
- 
+	<h3>Please enter the six to seven decimals for the latitude and longitude.</h3>
+	<form action="location-form">
+	Latitude: <input type="number" step="any" name="lat" placeholder="Latitude">
+	Longitude: <input type="number" step="any" name="lon" placeholder="Longitude">
+	<input type="submit" value="Find Weather">
+	</form>
+ 	
+ 	<br>
+ 	
 	<table border="1">
 		<thead>
 			<tr>
